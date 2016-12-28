5fd7f4cd9eddd500e7bc3311032973fdf4a3283d
IotaSpencer <iota@electrocode.net>
2016-12-27 21:17:25 -0500

    add init and setup scripts

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pyircs',
+      version='0.0.1',
+      description='Library to access the IRC-Source.com API.',
+      url='http://github.com/IRC-Source/pyircs',
+      author='Ken Spencer / Iota',
+      author_email='ken@electrocode.net',
+      license='MIT',
+      packages=['pyircs'],
+      zip_safe=False)
